a19325d0d45cafb26d20c110c86077083a3b0213
Gustavo Jacob <gustavogustavoribeiro2014@gmail.com>
2026-03-15 16:32:07 -0300

    fix: renomeia o diretorio

---
+++ src/main/java/br/edu/iff/forca/repository/RepositoryFactory.java
@@ -0,0 +1,5 @@
+package main.java.br.edu.iff.forca.repository;
+
+public class RepositoryFactory {
+    
+}
--- src/main/java/br/edu/iff/forca/respository/PalavraRepository.java
+++
@@ -1,5 +0,0 @@
-package main.java.br.edu.iff.forca.respository;
-
-public class PalavraRepository {
-    
-}
